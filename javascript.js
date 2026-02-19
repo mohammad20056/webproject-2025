8fa5ed2a8f4eef441f773f0be1885b0629897c9e
leroyvarghese2-png <leroyvarghese2@gmail.com>
2026-02-19 13:32:48 +0100

    java user story 2 pt 2

--- javascript.js
+++ javascript.js
@@ -15,3 +15,17 @@ const planten = [
     afbeelding: ""
   }
 ];
+const container = document.getElementById("assortiment");
+
+planten.forEach(plant => {
+  const plantCard = document.createElement("div");
+  plantCard.classList.add("plant-card");
+
+  plantCard.innerHTML = `
+    <img src="${plant.afbeelding}" alt="${plant.naam}">
+    <h2>${plant.naam}</h2>
+    <p>€ ${plant.prijs}</p>
+  `;
+
+  container.appendChild(plantCard);
+});
