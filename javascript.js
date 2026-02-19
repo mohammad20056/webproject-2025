9b7c2ba2c5ac5c27b967a3a93401f4810f2701ac
mohammad20056 <mohammadalmo2006@gmail.com>
2026-02-19 13:38:13 +0100

    opdracht

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const plants = [
+  {
+    id: 1,
+    name: "Monstera",
+    price: 24.99,
+    image: "images/monstera.jpg"
+  },
+  {
+    id: 2,
+    name: "Ficus",
+    price: 19.99,
+    image: "images/ficus.jpg"
+  },
+  {
+    id: 3,
+    name: "Cactus",
+    price: 9.99,
+    image: "images/cactus.jpg"
+  }
+];
