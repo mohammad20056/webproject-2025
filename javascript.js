72f31f5f27ec593f9e551689d0e28616dccc1b9e
leroyvarghese2-png <leroyvarghese2@gmail.com>
2026-03-05 13:11:03 +0100

    merge

--- javascript.js
+++ javascript.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-=======
-const plants = [
-  {
-    id: 1,
-    name: "Monstera",
-    price: 24.99,
-    image: "images/monstera.jpg"
-  },
-  {
-    id: 2,
-    name: "Ficus",
-    price: 19.99,
-    image: "images/ficus.jpg"
-  },
-  {
-    id: 3,
-    name: "Cactus",
-    price: 9.99,
-    image: "images/cactus.jpg"
-  }
-];
->>>>>>> 635531e2e7a4bc719a13fc75247df34e7bcc4b67
 const planten = [
   {
     naam: "cactus",
