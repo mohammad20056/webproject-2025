9bd0d3f1a9a9f0721c89ae349e6a560895374b27
leroyvarghese2-png <leroyvarghese2@gmail.com>
2026-03-05 12:47:43 +0100

    style en java

--- javascript.js
+++ javascript.js
@@ -1,42 +1,20 @@
-<<<<<<< HEAD
-=======
-const plants = [
-  {
-    id: 1,
-    name: "Monstera",
-    price: 24.99,
-    image: "images/monstera.jpg"
-  },
-  {
-    id: 2,
-    name: "Ficus",
-    price: 19.99,
-    image: "images/ficus.jpg"
-  },
-  {
-    id: 3,
-    name: "Cactus",
-    price: 9.99,
-    image: "images/cactus.jpg"
-  }
-];
->>>>>>> e6a9ac13930b2387597d572663b506b82ff239dd
 const planten = [
   {
     naam: "cactus",
     prijs: 5.99,
-    afbeelding: "cactus.jpg"
+    afbeelding: ""
   },
   {
     naam: "zonnebloem",
     prijs: 7.50,
-    afbeelding: "zonnebloem.jpg"
+    afbeelding: ""
   },
   {
     naam: "rode tulpen",
     prijs: 3.99,
-    afbeelding: "images/download.jpg"
-  }
+    afbeelding: ""
+  },
+   
 ];
 
 const container = document.getElementById("assortiment");
@@ -52,8 +30,4 @@ planten.forEach(plant => {
   `;
 
   container.appendChild(plantCard);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> e6a9ac13930b2387597d572663b506b82ff239dd
